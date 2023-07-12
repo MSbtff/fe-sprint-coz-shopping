77e74c3dfbead0d1cdb7d7fbc4612f91a9b37234
MS_btff <whdghks845@gmail.com>
2023-07-12 15:07:30 +0900

    coz-shopping

--- shopping/src/component/Header.jsx
+++ shopping/src/component/Header.jsx
@@ -1,16 +1,30 @@
 import React from 'react';
 import styled from 'styled-components';
+import {Link} from 'react-router-dom';
+import Logo from './Logo';
 
-const head = styled.div`
+const HeaderWrapper = styled.header`
+  position: relative;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
   width: 1280px;
   height: 80px;
+  margin: 0 auto;
 `;
 
-export default function Header() {
+const ShadowWrapper = styled.div`
+  box-shadow: 0px 8px 8px 0px #0000001a;
+`;
+
+function Header() {
   return (
-    <div>
-      <img src="" alt="" />
-      <img src="" alt="" />
-    </div>
+    <ShadowWrapper>
+      <HeaderWrapper>
+        <Logo />
+      </HeaderWrapper>
+    </ShadowWrapper>
   );
 }
+
+export default Header;
--- shopping/src/page/Main.jsx
+++ shopping/src/page/Main.jsx
@@ -1,14 +1,5 @@
 import React from 'react';
-import {createGlobalStyle} from 'styled-components';
 
-const GlobalStyle = createGlobalStyle`
-    *{
-        margin: 0;
-        box-sizing: border-box;
-
-    }
-`;
-
-export default function main() {
-  return <GlobalStyle></GlobalStyle>;
+export default function Main() {
+  return;
 }
